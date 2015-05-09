d90bfae31daba5f68ec6d6e9f6c395729c02e22b
dk021212 <dk021212@sina.com>
2015-05-09 14:50:25 +0800

    black

--- src/com/stone/ui/common/CommonProgressDialogFragment.java
+++ src/com/stone/ui/common/CommonProgressDialogFragment.java
@@ -1,5 +1,8 @@
 package com.stone.ui.common;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
@@ -15,5 +18,18 @@ public class CommonProgressDialogFragment extends DialogFragment {
 		bundle.putString("Content", content);
 		fragment.setArguments(bundle);
 		return fragment;
+	}
+	
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState){
+		ProgressDialog dialog=new ProgressDialog(getActivity());
+		dialog.setMessage(getArguments().getString("Content"));
+		return dialog;
+	}
+	
+	@Override
+	public void onCancel(DialogInterface dialog){
+		super.onCancel(dialog);
+		getActivity().finish();
 	}
 }
